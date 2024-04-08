f1ae4bd68ad5addf1453fd75d7ecf630b875b11b
Sergey Tsepodoy <sp1tfire_90@mail.ru>
2024-04-09 00:19:42 +0500

    feat: getIdGeneratorFunction

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -213,8 +213,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let incID = startFrom;
+  return () => {
+    const result = incID;
+    incID += 1;
+    return result;
+  };
 }
 
 module.exports = {
